28a4db9f90285d038db95bd2df5eeace84b6ed8f
DARWIN-coder-G7 <115872795+DARWIN-coder-G7@users.noreply.github.com>
2023-01-04 06:18:15 -0800

    Add files via upload

---
+++ shopping/src/data.js
@@ -0,0 +1,28 @@
+let shopItemData =[{
+    id:"one",
+    name:"veggie",
+    price: 25,
+    desc:"a good vegetable at affordable price",
+    img:"images/istockphoto-1306511017-1024x1024.jpg"
+},
+{
+id:"two",
+name:"greenie",
+price: 34,
+desc:"a good vegetable at affordable price",
+img:"images/annie-spratt-m1t-RJ1iCIU-unsplash.jpg"
+},
+{
+id:"three",
+name:"potato",
+price: 56,
+desc:"a good vegetable at affordable price",
+img:"images/annie-spratt-m1t-RJ1iCIU-unsplash.jpg"
+},
+{
+id:"four",
+name:"carrot",
+price: 52,
+desc:"a good vegetable at affordable price",
+img:"images/elianna-friedman-4jpNPu7IW8k-unsplash.jpg"
+}];
