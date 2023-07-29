658c88dbf204e6f04b790921f3cda914d8516cdf
DARWIN-coder-G7 <115872795+DARWIN-coder-G7@users.noreply.github.com>
2023-07-29 10:06:46 -0700

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/components/project.css';
 import '../styles/components/proj.css';
+import '../styles/components/education.css';
 import '../styles/components/contact.css';
 import '../styles/components/footer.css';
 import '../styles/components/mobile-nav.css';
